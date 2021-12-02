5cb8ee164b404143d1547c03dc80943b4117984f
TiagoVillarim <tiagovillarim@gmail.com>
2021-12-02 18:51:18 -0300

    projeto atualizado, mostrando itens na tela

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,27 +1,43 @@
-import React, { useState } from "react";
-import HomeStyle from "../styles/home.scss"
-import Modal from "../modal/modal"
+import React from "react";
 import { userContext } from "../context/useContext";
+import Modal from "../modal/modal"
+import HomeStyle from "../styles/home.scss"
+import GlobalStyle from "../../../src/Global.scss"
 
 export default function Home() {
 
   const { userList, showModal, setShowModal } = React.useContext(userContext);
 
+  console.log(userList)
+
 
   return(
     <>
       <header className="header">
-        <a className="title">Cadastro de usuários</a>
+        <h1 className="title">Cadastro de usuários</h1>
       </header>
 
       <body className="body-container">
         <button onClick={() => setShowModal(true)}>show modal</button>
         <Modal showModal={showModal} onClose={() => setShowModal(false)}/>
         <section className="users-section">
+          <div className="users-box">
+            <a>Name</a>
+            <a>Age</a>
+            <a>Job</a>
+            <a>Email</a>
+          </div>
           <div className="users-container">
-            {userList.map(listItem => (
-              <a className="users">{listItem}</a>
-              ))}
+            {userList.map((listItem, index) => {
+              return(
+                <div className="list-users" key={index}>
+                  <a>{listItem.users.name}</a>
+                  <a>{listItem.users.age}</a>
+                  <a>{listItem.users.job}</a>
+                  <a>{listItem.users.email}</a>
+                </div>
+              )
+            })}
           </div>
         </section>
       </body>
